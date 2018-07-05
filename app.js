a32613dc2d454100b4b1af54a6d521cb8872f9d8
Jason Asselin <asselin.jason@gmail.com>
2018-07-04 20:37:17 -0400

    Add users routes

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ const passport = require('passport');
 const moongoose = require('mongoose');
 
 const app = express();
+
+const users = require('./routes/users');
+
+// Port Number
 const port = 3000;
 
+// CORS Middleware
 app.use(cors());
 
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index Route
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 })
 
+// Start Server
 app.listen(port, () => {
     console.log(`Server started on port ${port}.`);
 });
