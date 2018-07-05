f9e0a7a75c6c28692b70aadd005b042756a6fdd8
Jason Asselin <asselin.jason@gmail.com>
2018-07-04 20:52:33 -0400

    Connect to MongoDB

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
-const moongoose = require('mongoose');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+
+// Connect to database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to database ${config.database}.`);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+    console.log(`Database error: ${err}.`);
+});
 
 const app = express();
 
@@ -14,6 +29,9 @@ const port = 3000;
 
 // CORS Middleware
 app.use(cors());
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
